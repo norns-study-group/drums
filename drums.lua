10e515b2dde4247d29764784e3cf1f6cf840f04a
Tyler Etters <tyler@etters.co>
2021-01-03 16:20:07 -0600

    start of a step sequencer

--- drums.lua
+++ drums.lua
@@ -1,11 +1,17 @@
--- drums
-
+-- k1: exit   e1: ???
+--
+--         e2: y     e3: x
+--    k2: change   k3: ???
+--
+fn = include("lib/functions")
 graphics = include("lib/graphics")
+Track = include("lib/track")
 Lattice = include("lib/lattice")
 
 function init()
   graphics.init()
   screen_dirty = true
+  selected_track = 1
   measure = 0
   lattice = Lattice:new()
   whole_notes = lattice:new_pattern{
@@ -16,6 +22,13 @@ function init()
     end
   }
   lattice:start()
+  tracks = {}
+  for i = 1, 7 do
+    local track = Track:new()
+    track:set_pattern(fn.make_random_pattern())
+    tracks[i] = track
+  end
+  tracks[1]:select()
   graphics_clock_id = clock.run(graphics_loop)
 end
 
@@ -30,15 +43,15 @@ end
 
 function redraw()
   graphics:setup()
-  graphics:rect(1, 1, 7, 64, 15)
-  graphics:text_rotate(7, 62, "DRUMS", -90, 0)
-  graphics:text(64, 32, measure, 15)
+  graphics:draw_title()
+  graphics:draw_tracks()
   graphics:teardown()
 end
 
 function graphics_loop()
   while true do
     clock.sleep(1/15)
+    graphics:modulate()
     if screen_dirty then
       redraw()
       screen_dirty = false
@@ -48,12 +61,20 @@ end
 
 function key(k, z)
   if z == 0 then return end
-  print(k, z, "???")
+  if k == 2 then
+    fn.get_selected_track():change()
+  elseif k == 3 then
+
+  end
   screen_dirty = true
 end
 
 function enc(e, d)
-  print(e, d, "???")
+  if e == 2 then
+    fn.select_track(d)
+  elseif e == 3 then
+    fn.select_track_index(d)
+  end
   screen_dirty = true
 end
 
---
+++ lib/functions.lua
@@ -0,0 +1,35 @@
+local fn = {}
+
+function fn.get_selected_track()
+  return tracks[selected_track]
+end
+
+function fn.select_track(d)
+  fn.get_selected_track():deselect()
+  selected_track = util.clamp(selected_track + d, 1, #tracks)
+  fn.get_selected_track():select()
+  fn.get_selected_track():set_selected_index(1)
+end
+
+function fn.select_track_index(d)
+  fn.get_selected_track():set_selected_index(fn.get_selected_track():get_selected_index() + d)
+end
+
+function fn.make_random_pattern(n)
+  local n = n ~= nil and n or math.random(3, 16)
+  local out = {}
+  for i = 1, n do
+    table.insert(out, math.random(1, 2) == 1)
+  end
+  return out
+end
+
+function rerun()
+  norns.script.load(norns.state.script)
+end
+
+function r()
+  norns.script.load(norns.state.script)
+end
+
+return fn
